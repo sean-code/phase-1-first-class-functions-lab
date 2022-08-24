bbd85486f57ab9a12bf3751a30789d85c790eeb2
sean-code <john.nganga@student.moringaschool.com>
2022-08-24 16:57:48 +0300

    Completed lab and all tests passing

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
-// Code your solution in this file!
-//let array = ['Tom', 'Cruise', 'Terry', 'Crews']
-// const returnFirstTwoDrivers() = function anonymousFunction([]){
-//     return (['Tom', 'Cruise']);
+// // Code your solution in this file!
+// //let array = ['Tom', 'Cruise', 'Terry', 'Crews']
+// // const returnFirstTwoDrivers() = function anonymousFunction([]){
+// //     return (['Tom', 'Cruise']);
+// // }
+
+// const  returnFirstTwoDrivers = function anonymousFunction([]){
+//     return (['Antonia', 'Nuru']);
 // }
+
+// const returnLastTwoDrivers = function anonymousFunction([]){
+//     return (["Amari", "Mo"])
+// }
+// const selectingDrivers =[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+// function createFareMultiplier(anInteger){
+//     return function(fare){
+//       return fare * anInteger; //multiply fare x integer
+//     }
+// }
+
 
 const  returnFirstTwoDrivers = function anonymousFunction([]){
     return (['Antonia', 'Nuru']);
-}
-
-const returnLastTwoDrivers = function anonymousFunction([]){
-    return (["Amari", "Mo"])
-}
-const selectingDrivers =[returnFirstTwoDrivers,returnLastTwoDrivers];
-
-function createFareMultiplier(anInteger){
-    return function(fare){
-      return fare * anInteger; //multiply fare x integer
+    
     }
-}
-
-
-const fareDoubler = createFareMultiplier(2)// fare double is x2
-
-const fareTripler = createFareMultiplier(3) //fare triple is x3\
-
-const selectingDifferentDrivers = function (arrayOfDrivers){
-    return driversToReturn(arrayOfDrivers);
-}
+    
+    const  returnLastTwoDrivers = function anonymousFunction([]){
+        return (['Amari', 'Mo']);
+    
+    }
+    const selectingDrivers =[returnFirstTwoDrivers,returnLastTwoDrivers];
+    
+    function createFareMultiplier(anInteger){
+        return function(fare){
+          return fare*anInteger;
+        };
+    }
+    const fareDoubler = createFareMultiplier(2);
+    const fareTripler = createFareMultiplier(3);
+    
+    const selectDifferentDrivers = function (arrayOfDrivers, driversToReturn) {
+        return driversToReturn(arrayOfDrivers);
+      };
